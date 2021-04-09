b65d4d74e843b90b87e772d04c4524c7800ebc1d
fourneau <cathyjulien14@orange.fr>
2021-04-09 11:47:58 +0200

    src_new

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,40 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: aurelwcs
+ * Date: 08/04/19
+ * Time: 18:40
+ */
+
+namespace App\Controller;
+
+class HomeController extends AbstractController
+{
+    public array $products = [
+        'product1',
+        'product2',
+        'product3',
+        'product4',
+        'product5'
+    ];
+    /**
+     * Display home page
+     *
+     * @return string
+     * @throws \Twig\Error\LoaderError
+     * @throws \Twig\Error\RuntimeError
+     * @throws \Twig\Error\SyntaxError
+     */
+    public function index()
+    {
+        return $this->twig->render('Home/index.html.twig');
+    }
+
+    public function showProducts()
+    {
+        return $this->twig->render('Products/products.html.twig', [
+            "products" => $this->products
+        ]);
+    }
+}
